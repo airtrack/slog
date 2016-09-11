e27efcd9098577dd5dd45e206c0dac5c177995ca
airtrack <airtrack.sk@gmail.com>
2016-09-11 21:58:41 +0800

    Modify test example.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ Slog
 ====
 
 A simple log library written in C++ 11.
+
+Platforms
+---------
+
+* Mac OSX(clang)
+* Linux(gcc)
+
+Features
+--------
+
+* Header Only
+* Self defined struct formatting
+* Rotating log files
--- Test.cpp
+++ Test.cpp
@@ -1,11 +1,28 @@
 #include "Logger.h"
 
+struct Test
+{
+    int i;
+    int j;
+};
+
+inline slog::ArgFormatter & operator << (slog::ArgFormatter &f, const Test &t)
+{
+    f << "{ i: " << t.i << ", j: " << t.j << " }";
+    return f;
+}
+
 int main()
 {
     slog::SyncLogger logger("test.log", 200 * 1024 * 1024, 1);
 
     for (int i = 0; i < 1000000; ++i)
         logger.Info("[slog] message #{} : This is some text for your pleasure", i);
+
+    Test t1{ 1, 2 };
+    Test t2{ 3, 4 };
+
+    logger.Info("{}, {}", t1, t2);
 
     return 0;
 }
